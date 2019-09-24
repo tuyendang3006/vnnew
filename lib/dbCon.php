a2d7c7a6cb6429c17c6d68862ca205f9ee667517
tuyendang3006 <48243406+tuyendang3006@users.noreply.github.com>
2019-09-24 18:33:57 +0200

    Update dbCon.php

--- lib/dbCon.php
+++ lib/dbCon.php
@@ -1,8 +1,5 @@
 <?php
 $con = mysqli_connect("localhost", "root", "");
 mysqli_select_db($con, "vnnew");
-
-// $con = mysqli_connect("sql213.byethost7.com", "b7_24537340", "tuyen1994");
-// mysqli_select_db($con, "b7_24537340_jd");
 mysqli_query($con, "SET NAMES 'utf8' ");
 ?>
